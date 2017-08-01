9d5c7a3b8511fb76d4eb4ac819963fa937b2c01a
MrElektronix <milovanpelt@gmail.com>
2017-08-01 14:39:27 +0200

    update

---
+++ Assets/Inventory.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Inventory : MonoBehaviour {
+
+    private List<Item> items = new List<Item>();
+
+	// Use this for initialization
+	void Start () {
+        Item newItem = new Item("Sword", Item.ItemType.Weapon);
+        Item newItem2 = new Item("Potion", Item.ItemType.Potion);
+        AddItem(newItem);
+        AddItem(newItem2);
+
+        for (int i = 0; i < items.Count; i++)
+        {
+            Debug.Log(items[i].Name);
+        }
+	}
+	
+	// Update is called once per frame
+	void AddItem (Item item) {
+        items.Add(item);
+	}
+}
---
+++ Assets/Item.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class Item {
+
+    public enum ItemType
+    {
+        Weapon,
+        Potion
+    }
+
+    private ItemType type;
+
+    public ItemType Type
+    {
+        get { return type; }
+    }
+
+
+    private string name;
+    public string Name {
+        get { return name; }
+    }
+	// Use this for initialization
+
+    public Item(string name, ItemType type)
+    {
+        //Debug.Log("Hallo ik ben een constructor");
+        this.name = name;
+        this.type = type;
+    }
+   
+	void Start () {
+        Debug.Log("Hallo ik ben start");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
